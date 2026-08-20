d365427a793ad7b440fba902168ca82b0315f961
Nirav Rajeshkumar Jani <159779464+Niravjani@users.noreply.github.com>
2026-08-20 14:16:27 +0530

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "My Practice",
+    "version": "1.0",
+    "summary": "Practice Module",
+    "author": "Nirav1",
+    "category": "Training",
+    "license": "LGPL-3",
+    "depends": ["base", "website"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/course_views.xml",
+        "views/attendance_views.xml",
+        "views/menu.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
